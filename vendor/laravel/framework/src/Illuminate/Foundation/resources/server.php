2b75b1e0aff58ce2cc5c86d81368bd569b188cd6
daposadap9 <diegoposada1999@gmail.com>
2024-09-07 15:43:54 -0500

    new changes

--- vendor/laravel/framework/src/Illuminate/Foundation/resources/server.php
+++ vendor/laravel/framework/src/Illuminate/Foundation/resources/server.php
@@ -1,11 +1,9 @@
 <?php
 
-require __DIR__.'C:\xampp\htdocs\pet-registry\vendor\autoload.php';
-
 use Illuminate\Support\Facades\Artisan;
 
-$port = getenv('PORT') ?: 8000;
-$host = getenv('HOST') ?: '0.0.0.0';
+$port = env('PORT', 8080);
+$host = env('HOST', '0.0.0.0');
 Artisan::call('serve', [
     '--host' => $host,
     '--port' => $port,
@@ -29,4 +27,3 @@ $remoteAddress = $_SERVER['REMOTE_ADDR'].':'.$_SERVER['REMOTE_PORT'];
 file_put_contents('php://stdout', "[$formattedDateTime] $remoteAddress [$requestMethod] URI: $uri\n");
 
 require_once $publicPath.'/index.php';
-
